a46bb2728bcd28ba57fa8110e05e9e79768cc3b4
CodingCherry <cherry.alexeeva@gmail.com>
2019-01-21 16:48:48 +0700

    UITableView, data source and delegate

--- coder-swag/Services/DataService.swift
+++ coder-swag/Services/DataService.swift
@@ -13,10 +13,12 @@ class DataService {
     
     private let categories = [
     Category(title: "SHIRTS", imageName: "shirts.png"),
-    Category(title: "HOODIES", imageName: "hoodies.png")
+    Category(title: "HOODIES", imageName: "hoodies.png"),
+    Category(title: "HATS", imageName: "hats.png"),
+    Category(title: "DIGITAL", imageName: "digital.png")
     ]
     
-    func getCategories() {
-        
+    func getCategories() -> [Category] {
+        return categories
     }
 }
