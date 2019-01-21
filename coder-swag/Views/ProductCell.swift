293f66c2cc87995f2b4aa01abce5b9fdeeb53631
CodingCherry <cherry.alexeeva@gmail.com>
2019-01-21 22:09:08 +0700

    UITableViewCell set

--- coder-swag/Views/ProductCell.swift
+++ coder-swag/Views/ProductCell.swift
@@ -10,7 +10,13 @@ import UIKit
 
 class ProductCell: UICollectionViewCell {
     @IBOutlet weak var productImage: UIImageView!
-    @IBOutlet weak var productLabel: UILabel!
+    @IBOutlet weak var productTitle: UILabel!
     @IBOutlet weak var productPrice: UILabel!
+    
+    func updateViews(product: Product) {
+        productImage.image = UIImage(named: product.imageName)
+        productTitle.text = product.title
+        productPrice.text = product.price
+    }
     
 }
