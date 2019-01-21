b723de36a1b1930ca614ef3f54ed7cb8b9130777
CodingCherry <cherry.alexeeva@gmail.com>
2019-01-21 17:21:39 +0700

    UICollectionView created

---
+++ coder-swag/Views/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Alexeeva Ekaterina on 1/21/19.
+//  Copyright © 2019 Alexeeva Ekaterina. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productLabel: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
